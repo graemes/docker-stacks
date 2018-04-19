87952c720a32107b88cbe7d4f0e41bcc0fbeaa4d
graemes <graemes@graemes.com>
2018-04-19 15:14:30 +0000

    Create local branch

---
+++ build-all.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+pushd ./base-notebook/ ; ./build-docker.sh ; popd
+pushd ./minimal-notebook/ ; ./build-docker.sh ; popd
+pushd ./scipy-notebook/ ; ./build-docker.sh ; popd
+pushd ./datascience-notebook/ ; ./build-docker.sh ; popd
+
+#./examples/docker-compose/notebook/build.sh
---
+++ datascience-notebook/build-docker.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+docker pull nvidia/cuda:9.1-base
+docker pull nvidia/cuda:9.1-runtime
+docker pull nvidia/cuda:9.1-devel
+docker pull registry.graemes.com/graemes/jupyter-scipy-notebook:latest
+
+docker build . -t registry.graemes.com/graemes/jupyter-datascience-notebook:latest  
+docker push registry.graemes.com/graemes/jupyter-datascience-notebook:latest
+
