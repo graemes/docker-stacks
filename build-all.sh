df5e815b7ffc718b7f2e29d95e563d2bf6b775e0
graemes <graemes@graemes.com>
2018-04-20 21:32:41 +0000

    Add ultracat version

--- build-all.sh
+++ build-all.sh
@@ -4,5 +4,6 @@ pushd ./base-notebook/ ; ./build-docker.sh ; popd
 pushd ./minimal-notebook/ ; ./build-docker.sh ; popd
 pushd ./scipy-notebook/ ; ./build-docker.sh ; popd
 pushd ./datascience-notebook/ ; ./build-docker.sh ; popd
+pushd ./ultracat-notebook/ ; ./build-docker.sh ; popd
 
 #./examples/docker-compose/notebook/build.sh
