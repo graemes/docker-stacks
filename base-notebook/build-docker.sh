0e7fe071c0552713f68bb7fd5cf22c2749406d70
graemes <graemes@github>
2018-06-29 09:44:07 +0000

    Remove cuda pull from build-scripts

--- base-notebook/build-docker.sh
+++ base-notebook/build-docker.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-#docker pull nvidia/cuda:9.1-base
-#docker pull nvidia/cuda:9.1-runtime
-#docker pull nvidia/cuda:9.1-devel
-
 docker build . -t registry.graemes.com/graemes/jupyter-base-notebook:latest  
 docker push registry.graemes.com/graemes/jupyter-base-notebook:latest
 
--- scipy-notebook/build-docker.sh
+++ scipy-notebook/build-docker.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-docker pull nvidia/cuda:9.1-base
-docker pull nvidia/cuda:9.1-runtime
-docker pull nvidia/cuda:9.1-devel
 docker pull registry.graemes.com/graemes/jupyter-minimal-notebook:latest
 
 docker build . -t registry.graemes.com/graemes/jupyter-scipy-notebook:latest  
