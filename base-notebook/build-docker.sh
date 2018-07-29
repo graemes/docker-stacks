0a183404ac900030bbac702faf45c2561e6734de
graemes <graemes@github>
2018-07-29 14:10:44 +0000

    Local tweaks

--- base-notebook/build-docker.sh
+++ base-notebook/build-docker.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+docker pull ubuntu:18.04
+
 docker build . -t registry.graemes.com/graemes/jupyter-base-notebook:latest  
 docker push registry.graemes.com/graemes/jupyter-base-notebook:latest
 
