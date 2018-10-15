dd9cd43af8b3fed0ab14c1c328066f8c260bf3e1
graemes <graemes@github>
2018-10-15 21:48:53 +0100

    Merge components of 'batteries included' container into single file

--- build-all.sh
+++ build-all.sh
@@ -2,6 +2,7 @@
 
 pushd ./base-notebook/ ; ./build-docker.sh ; popd
 pushd ./minimal-notebook/ ; ./build-docker.sh ; popd
+pushd ./minimal-notebook-gpu/ ; ./build-docker.sh ; popd
 pushd ./scipy-notebook/ ; ./build-docker.sh ; popd
 pushd ./datascience-notebook/ ; ./build-docker.sh ; popd
 pushd ./ultracat-notebook/ ; ./build-docker.sh ; popd
