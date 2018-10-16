c75755cbe777a6376b17a028f88bf3d0567dc3f2
graemes <graemes@github>
2018-10-16 09:37:48 +0100

    Amend remaining builds

--- build-all.sh
+++ build-all.sh
@@ -2,12 +2,8 @@
 
 pushd ./base-notebook/ ; ./build-docker.sh ; popd
 pushd ./minimal-notebook/ ; ./build-docker.sh ; popd
-<<<<<<< HEAD
-#pushd ./minimal-notebook-gpu/ ; ./build-docker.sh ; popd
-=======
->>>>>>> parent of dd9cd43... Merge components of 'batteries included' container into single file
 pushd ./scipy-notebook/ ; ./build-docker.sh ; popd
 pushd ./datascience-notebook/ ; ./build-docker.sh ; popd
-#pushd ./ultracat-notebook/ ; ./build-docker.sh ; popd
+pushd ./ultracat-notebook/ ; ./build-docker.sh ; popd
 
 #./examples/docker-compose/notebook/build.sh
--- scipy-notebook/build-docker.sh
+++ scipy-notebook/build-docker.sh
@@ -4,17 +4,17 @@ BASE_REGISTRY="registry.graemes.com/graemes"
 BASE_CONTAINER="jupyter-minimal-notebook"
 OUTPUT_BASE="jupyter-scipy-notebook"
 
-docker pull ${BASE_REGISTRY}/${BASE_CONTAINER}:latest
-docker pull ${BASE_REGISTRY}/${BASE_CONTAINER}-gpu:latest
+docker pull ${BASE_REGISTRY}/${BASE_CONTAINER}
+docker pull ${BASE_REGISTRY}/${BASE_CONTAINER}:gpu
 
 docker build . \
-    -t ${BASE_REGISTRY}/${OUTPUT_BASE}:latest \
-    --build-arg BUILD_CONTAINER=${BASE_REGISTRY}/${BASE_CONTAINER}:latest
-docker push ${BASE_REGISTRY}/${OUTPUT_BASE}:latest
+    -t ${BASE_REGISTRY}/${OUTPUT_BASE} \
+    --build-arg BUILD_CONTAINER=${BASE_REGISTRY}/${BASE_CONTAINER}
+docker push ${BASE_REGISTRY}/${OUTPUT_BASE}
 
 docker build . \
-    -t ${BASE_REGISTRY}/${OUTPUT_BASE}-gpu:latest \
-    --build-arg BUILD_CONTAINER=${BASE_REGISTRY}/${BASE_CONTAINER}-gpu:latest
-docker push ${BASE_REGISTRY}/${OUTPUT_BASE}-gpu:latest
+    -t ${BASE_REGISTRY}/${OUTPUT_BASE}:gpu \
+    --build-arg BUILD_CONTAINER=${BASE_REGISTRY}/${BASE_CONTAINER}:gpu
+docker push ${BASE_REGISTRY}/${OUTPUT_BASE}:gpu
 
 
