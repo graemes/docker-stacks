6722491111427bb7dadf708cfb0dace8d14e7d03
graemes <graemes@github>
2018-10-16 07:48:22 +0100

    Change scipy to use build-arg for FROM

--- scipy-notebook/build-docker.sh
+++ scipy-notebook/build-docker.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
-docker pull registry.graemes.com/graemes/jupyter-minimal-notebook:latest
+BASE_REGISTRY="registry.graemes.com/graemes"
+BASE_CONTAINER="jupyter-minimal-notebook"
+OUTPUT_BASE="jupyter-scipy-notebook"
 
-docker build . -t registry.graemes.com/graemes/jupyter-scipy-notebook:latest  
-docker push registry.graemes.com/graemes/jupyter-scipy-notebook:latest
+docker pull ${BASE_REGISTRY}/${BASE_CONTAINER}:latest
+docker pull ${BASE_REGISTRY}/${BASE_CONTAINER}-gpu:latest
+
+docker build . \
+    -t ${BASE_REGISTRY}/${OUTPUT_BASE}:latest
+docker push ${BASE_REGISTRY}/${OUTPUT_BASE}:latest
+
+docker build . \
+    -t ${BASE_REGISTRY}/${OUTPUT_BASE}-gpu:latest \
+    --build-arg BUILD_CONTAINER=${BASE_REGISTRY}/${BASE_CONTAINER}-gpu:latest
+docker push ${BASE_REGISTRY}/${OUTPUT_BASE}-gpu:latest
+
 
