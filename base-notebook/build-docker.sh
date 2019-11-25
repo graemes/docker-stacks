23e61bef3762e90dcd79a6b78646ee01cf6402f8
Graeme Seaton <graemes@graemes.com>
2019-11-25 19:47:21 +0000

    Use source for cuda version

--- base-notebook/build-docker.sh
+++ base-notebook/build-docker.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
+. ${HOME}/build/tools/build-params
+
 BASE_REGISTRY="registry.graemes.com/graemes"
 BASE_CONTAINER=${BASE_REGISTRY}/"ubuntubuild:18.04"
 BASE_OUTPUT=${BASE_REGISTRY}/"jupyter/base-notebook"
-GPU_CONTAINER=${BASE_REGISTRY}/"nvidia-cuda:10.1-devel"
+GPU_CONTAINER=${BASE_REGISTRY}/"nvidia-cuda:${CUDA_VERSION}-devel"
 GPU_OUTPUT="${BASE_OUTPUT}:gpu"
 
 docker pull ${BASE_CONTAINER}
