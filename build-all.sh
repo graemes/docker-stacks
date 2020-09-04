7accf9d25f52d082e08b9170a0607f82f2a3d50f
Graeme Seaton <graemes@graemes.com>
2020-09-04 20:47:15 +0000

    Reduce set of built containers

--- build-all.sh
+++ build-all.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 for NOTEBOOK in "base-notebook" "minimal-notebook" "scipy-notebook" "datascience-notebook" \
-	        "all-notebook" "tensorflow-notebook" "r-notebook" \
-		"pyspark-notebook" "all-spark-notebook"
+	        "all-notebook" "r-notebook" 
+#	        "all-notebook" "tensorflow-notebook" "r-notebook" \
+#		"pyspark-notebook" "all-spark-notebook"
 do
 	pushd ${NOTEBOOK}
 	./build-docker.sh
