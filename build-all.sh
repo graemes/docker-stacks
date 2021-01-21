1afc828954ed9c106b95f33333cead4a55f3d3b1
Graeme Seaton <graemes@graemes.com>
2021-01-21 23:04:52 +0000

    Build both classes in parallel

--- build-all.sh
+++ build-all.sh
@@ -2,20 +2,25 @@
 
 NOTEBOOKS="base-notebook minimal-notebook scipy-notebook datascience-notebook tensorflow-notebook r-notebook pyspark-notebook all-notebook all-spark-notebook"
 
-for NOTEBOOK in ${NOTEBOOKS}
-do
-	pushd ${NOTEBOOK}
-	./build-docker-cpu.sh
-	popd
-done
+function build-all-cpu() {
+  for NOTEBOOK in ${NOTEBOOKS}
+  do
+    pushd ${NOTEBOOK}
+    ./build-docker-cpu.sh 
+    popd
+  done
+}
 
-docker-clean-unused.sh
+function build-all-gpu() {
+  for NOTEBOOK in ${NOTEBOOKS}
+  do
+    pushd ${NOTEBOOK}
+    ./build-docker-gpu.sh 
+    popd
+  done
+}
 
-for NOTEBOOK in ${NOTEBOOKS}
-do
-	pushd ${NOTEBOOK}
-	./build-docker-gpu.sh
-	popd
-done
-
+build-all-cpu &
+build-all-gpu &
+wait
 docker-clean-unused.sh
