fed6480cc3df47c135e67fc65d4fcb6b2cc642cc
Graeme Seaton <graemes@graemes.com>
2021-02-27 12:59:44 +0000

    Add prune

--- build-all.sh
+++ build-all.sh
@@ -7,6 +7,7 @@ function build-all-cpu() {
   do
     pushd ${NOTEBOOK}
     ./build-docker-cpu.sh 
+    docker system prune -f
     popd
   done
 }
@@ -16,6 +17,7 @@ function build-all-gpu() {
   do
     pushd ${NOTEBOOK}
     ./build-docker-gpu.sh 
+    docker system prune -f
     popd
   done
 }
